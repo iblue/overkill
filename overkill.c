1d0b805dbef9da25f1cb01dc769177ea844a9d04
Markus Fenske <iblue@gmx.net>
2014-06-08 16:07:08 +0200

    Deshaking PoC

--- overkill.c
+++ overkill.c
@@ -90,6 +90,37 @@ int main(int argc, char **argv) {
           printf("Frame %d, Feature %d at %d, %d. FAIL: %d\n", current_frame, i, location.x, location.y, marker_fail);
         }
 
+        /* Signed ints will be transformed to floats anyway, so we can do this
+         * directly without performance loss */
+
+        /* Calculate transformation to target */
+        CvPoint2D32f targets[] = {
+          cvPoint2D32f(640.0, 360.0),   /* Axis */
+          cvPoint2D32f(433.0, 360.0),   /* Top Center */
+          cvPoint2D32f(454.0, 467.0),   /* Edge Left */
+          cvPoint2D32f(447.0, 253.0),   /* Edge Right */
+          cvPoint2D32f(815.0, 446.0),   /* Bottom Left */
+          cvPoint2D32f(810.0, 253.0),   /* Bottom Right */
+        };
+        CvMat to = cvMat(6, 1, CV_32FC2, targets);
+
+        // Matrix contains 2D vectors as channels (who designed this shit?)
+        CvPoint2D32f points[6];
+        for(int i=0;i<6;i++) {
+          points[i] = cvPoint2D32f((double) last_location[i].x,
+                                   (double) last_location[i].y);
+        }
+        CvMat from = cvMat(6, 1, CV_32FC2, points);
+
+        CvMat *transformation = cvCreateMat(2,3, CV_32FC1);
+
+        cvEstimateRigidTransform(&from, &to, transformation, 0);
+
+        /* FIXME: Overkill - we just need the same type */
+        IplImage *dst = cvCloneImage(frame);
+        cvWarpAffine(frame, dst, transformation, CV_INTER_LINEAR+CV_WARP_FILL_OUTLIERS, CV_RGB(0,0,0));
+
+
         /*CvPoint axis = matchFeature(frame, FEATURE_AXIS);
         axis.x -= 10;
         CvPoint topCenter = matchFeature(frame, FEATURE_TOP_CENTER);
@@ -98,7 +129,7 @@ int main(int argc, char **argv) {
         cvCircle(frame, axis, radius, CV_RGB(0,255,255), 1, CV_AA, 0);*/
 
         /* display frame into window */
-        cvShowImage("Overkill", frame);
+        cvShowImage("Overkill", dst);
 
         /* And output to file */
         cvWriteFrame(output, frame);
