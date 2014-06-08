9e4dce490068c3fe009cd62df79a3b39bdca829f
Markus Fenske <iblue@gmx.net>
2014-06-08 18:44:11 +0200

    Add tracking point finder masks

--- mask.c
+++ mask.c
@@ -20,6 +20,16 @@ IplImage* mask(IplImage *source) {
   cvRectangle(ret, cvPoint(490, 450), cvPoint(750, 380), color, 1, CV_AA, 0);
 
   /* FIXME: Does not belong here! */
+  color = CV_RGB(255,255,255);
+  IplImage* temp_mask = cvCreateImage(cvGetSize(source), IPL_DEPTH_8U, 1);
+
+  /* Outer tracking zone */
+  cvRectangle(temp_mask, cvPoint(420, 610), cvPoint(800, 470), color, -1, CV_AA, 0);
+  cvRectangle(temp_mask, cvPoint(420, 240), cvPoint(800, 100), color, -1, CV_AA, 0);
+
+  /* Inner tracking zone */
+  cvRectangle(temp_mask, cvPoint(490, 260), cvPoint(750, 320), color, -1, CV_AA, 0);
+  cvRectangle(temp_mask, cvPoint(490, 450), cvPoint(750, 380), color, -1, CV_AA, 0);
 
   /* Convert to grey for corner tracking */
   IplImage* temp_grey = cvCreateImage(cvGetSize(source), IPL_DEPTH_8U, 1);
@@ -28,12 +38,13 @@ IplImage* mask(IplImage *source) {
   #define MAX_CORNERS 20
   CvPoint2D32f corners[MAX_CORNERS];
   int corner_count=MAX_CORNERS;
-  cvGoodFeaturesToTrack(temp_grey, NULL, NULL, corners, &corner_count, 0.01, 2, NULL, 3, 0, 0.04);
+  cvGoodFeaturesToTrack(temp_grey, NULL, NULL, corners, &corner_count, 0.20, 10, temp_mask, 3, 1, 0.04);
   cvReleaseImage(&temp_grey);
+  cvReleaseImage(&temp_mask);
 
   /* Highlight corners */
   for(int i=0;i<corner_count;i++) {
-    cvCircle(ret, cvPointFrom32f(corners[i]), 2, CV_RGB(0,0,0), 1, CV_AA, 0);
+    cvCircle(ret, cvPointFrom32f(corners[i]), 2, CV_RGB(255,255,255), 1, CV_AA, 0);
   }
 
   return ret;
