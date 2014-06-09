1d072e3a040b2561bfc416b402474ca845b72b70
Markus Fenske <iblue@gmx.net>
2014-06-09 10:13:04 +0200

    Tracking mask

--- mask.c
+++ mask.c
@@ -12,33 +12,41 @@ IplImage* mask(IplImage *source) {
 
   /* Outer tracking zone */
   color = CV_RGB(255,0,255);
-  cvRectangle(ret, cvPoint(420, 610), cvPoint(800, 470), color, 1, CV_AA, 0);
-  cvRectangle(ret, cvPoint(420, 240), cvPoint(800, 100), color, 1, CV_AA, 0);
+  cvRectangle(ret, cvPoint(420, 610), cvPoint(800, 470), color, 1, 8, 0);
+  cvRectangle(ret, cvPoint(420, 240), cvPoint(800, 100), color, 1, 8, 0);
 
   /* Inner tracking zone */
-  cvRectangle(ret, cvPoint(490, 260), cvPoint(750, 320), color, 1, CV_AA, 0);
-  cvRectangle(ret, cvPoint(490, 450), cvPoint(750, 380), color, 1, CV_AA, 0);
+  cvRectangle(ret, cvPoint(490, 260), cvPoint(750, 320), color, 1, 8, 0);
+  cvRectangle(ret, cvPoint(490, 450), cvPoint(750, 380), color, 1, 8, 0);
 
   /* FIXME: Does not belong here! */
-  color = CV_RGB(255,255,255);
+  CvScalar notrack = CV_RGB(0,0,0);
+  CvScalar track   = CV_RGB(255,255,255);
   IplImage* temp_mask = cvCreateImage(cvGetSize(source), IPL_DEPTH_8U, 1);
 
+  /* Initialize mask first. Everything non-zero will be tracked */ 
+  for(int i=0;i<temp_mask->imageSize;i++) {
+    temp_mask->imageData[i] = 0;
+  }
+
   /* Outer tracking zone */
-  cvRectangle(temp_mask, cvPoint(420, 610), cvPoint(800, 470), color, -1, CV_AA, 0);
-  cvRectangle(temp_mask, cvPoint(420, 240), cvPoint(800, 100), color, -1, CV_AA, 0);
+  cvRectangle(temp_mask, cvPoint(420, 610), cvPoint(800, 470), track, -1, 8, 0);
+  cvRectangle(temp_mask, cvPoint(420, 240), cvPoint(800, 100), track, -1, 8, 0);
 
   /* Inner tracking zone */
-  cvRectangle(temp_mask, cvPoint(490, 260), cvPoint(750, 320), color, -1, CV_AA, 0);
-  cvRectangle(temp_mask, cvPoint(490, 450), cvPoint(750, 380), color, -1, CV_AA, 0);
+  cvRectangle(temp_mask, cvPoint(490, 260), cvPoint(750, 320), track, -1, 8, 0);
+  cvRectangle(temp_mask, cvPoint(490, 450), cvPoint(750, 380), track, -1, 8, 0);
+  cvRectangle(temp_mask, cvPoint(490, 450), cvPoint(750, 380), notrack, -1, 8, 0);
 
   /* Convert to grey for corner tracking */
   IplImage* temp_grey = cvCreateImage(cvGetSize(source), IPL_DEPTH_8U, 1);
   cvCvtColor(source, temp_grey, CV_RGB2GRAY);
 
-  #define MAX_CORNERS 20
+  #define MAX_CORNERS 40
   CvPoint2D32f corners[MAX_CORNERS];
   int corner_count=MAX_CORNERS;
-  cvGoodFeaturesToTrack(temp_grey, NULL, NULL, corners, &corner_count, 0.20, 10, temp_mask, 3, 1, 0.04);
+  cvGoodFeaturesToTrack(temp_grey, NULL, NULL, corners, &corner_count, 0.01,
+      20, temp_mask, 3, 1, 0.04);
   cvReleaseImage(&temp_grey);
   cvReleaseImage(&temp_mask);
 
