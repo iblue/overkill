a1a76ce5113599e1833ebf2d0f51f168ccf4a735
Markus Fenske <iblue@gmx.net>
2014-06-09 12:18:05 +0200

    Comments

--- overkill.c
+++ overkill.c
@@ -14,6 +14,7 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
+    /* Initialize static feature detection */
     initFeatures(argv[3]);
 
     /* Create a window */
@@ -37,25 +38,29 @@ int main(int argc, char **argv) {
         /* exit loop if fram is null / movie end */
         if(!frame) break;
 
+        /* track static features into global variable */
         trackFeatures(frame, current_frame);
 
+        /* deshake using static features */
         IplImage *deshaked_frame = cvCreateImage(cvSize(frame->width,frame->height),
           frame->depth, frame->nChannels);
-
-        IplImage *target;
-
         deshake(frame, deshaked_frame);
+
+        /* Create mask for dynamic feature detection, display mask in target */
+        IplImage *target;
         IplImage *tracking_mask = mask(deshaked_frame, &target);
 
+        /* Detect dynamic features in mask */
         int corner_count = 30;
         CvPoint2D32f corners[corner_count];
         findTrackingPoints(deshaked_frame, tracking_mask, &corner_count, corners);
 
-        /* Highlight corners */
+        /* Highlight detected features */
         for(int i=0;i<corner_count;i++) {
           cvCircle(target, cvPointFrom32f(corners[i]), 2, CV_RGB(255,255,255), 1, CV_AA, 0);
         }
 
+        /* Free mem */
         cvReleaseImage(&tracking_mask);
         cvReleaseImage(&deshaked_frame);
 
@@ -63,14 +68,16 @@ int main(int argc, char **argv) {
         cvShowImage("Overkill", target);
         cvWriteFrame(output, target);
 
+        /* Free target mem */
         cvReleaseImage(&target);
+        /* Frame is released automatically */
 
         /* if ESC or q is pressed then exit loop */
         char c = cvWaitKey(33);
         if(c==27 || c == 'q') break;
     }
 
-    /* destroy pointer to video */
+    /* destroy pointer to capturing */
     cvReleaseCapture(&capture);
 
     /* delete window */
